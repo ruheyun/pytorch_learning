1feb52bad043bbb5e244e433b34eaea6fe9051ee
ruheyun <1654033661@qq.com>
2026-06-11 22:07:44 +0800

    202606111

--- utils/data_operation.py
+++ utils/data_operation.py
@@ -1,3 +1,5 @@
+import math
+import numpy as np
 
 
 def calculate_entropy(y):
@@ -5,4 +7,5 @@ def calculate_entropy(y):
     Calulate the entropy of label array y
     """
 
-    
+    log2 = lambda x: math.log(x) / math.log(2)
+    unique_labels = np.unique(y)
