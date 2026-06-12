d181bda22762a99369de18db9cd0f63e1b8ce4d1
ruheyun <1654033661@qq.com>
2026-06-12 11:07:14 +0800

    20260612q

--- utils/data_operation.py
+++ utils/data_operation.py
@@ -4,8 +4,88 @@ import numpy as np
 
 def calculate_entropy(y):
     """
-    Calulate the entropy of label array y
+    Calculate the entropy of label array y
     """
 
     log2 = lambda x: math.log(x) / math.log(2)
     unique_labels = np.unique(y)
+    entropy = 0
+    for label in unique_labels:
+        count = len(y[y==label])
+        p = count / len(y)
+        entropy += -p * log2(p)
+
+    return entropy
+
+
+def calculate_entropy_(y):
+    """
+    Calculate the entropy of label array y
+    """
+
+    entropy = 0
+    for label in np.unique(y):
+        p = np.sum(y == label) / len(y)
+        entropy -= p * math.log2(p)
+
+    return entropy
+
+
+def mean_squared_error(y_true, y_pred):
+    """
+    Returns the mean squared error between y_true and y_pred
+    """
+
+    mse = np.mean(np.power(y_true - y_pred, 2))
+
+    return mse
+
+
+def calculate_variance(X):
+    """
+    Return the variance of the features in dataset X
+    """
+
+    mean = np.ones(np.shape(X)) * X.mean(axis=0)
+    n_samples = np.shape(X)[0]
+    variance = (1 / n_samples) * np.diag((X - mean).T.dot(X - mean))
+
+    return variance
+
+
+def calculate_std_dev(X):
+    """
+    Calculate the standard deviations of the features in dataset X
+    """
+
+    std_dev = np.sqrt(calculate_variance(X))
+
+    return std_dev
+
+
+def euclidean_distance(x_1, x_2):
+    """
+    Calculates the l2 distance between two vectors
+    """
+
+    assert len(x_1) == len(x_2), 'Length of list or numpy is different.'
+
+    distance = 0
+    for i, j in zip(x_1, x_2):
+        distance += pow((i - j), 2)
+
+    return math.sqrt(distance)
+
+
+def accuracy_score(y_true, y_pred):
+    """
+    Compare y_true to y_pred and return the accuracy
+    """
+
+    accuracy = np.sum(y_true == y_pred, axis=0) / len(y_true)
+
+    return accuracy
+
+
+if __name__ == '__main__':
+    euclidean_distance([1, 2, 3], [4, 5, 6])
