a73f70daf8ba03a21dc16d1307794cb3c4678c9d
ruheyun <1654033661@qq.com>
2026-06-15 21:40:06 +0800

    20260615w

--- models/regression.py
+++ models/regression.py
@@ -63,6 +63,58 @@ class Regression(object):
         learning_rate: float. The step length that will be used when updating the weights.
     """
 
+    def __init__(self, n_iterations, learning_rate):
+        self.n_iterations = n_iterations
+        self.learning_rate = learning_rate
+
+    def initialize_weights(self, n_features):
+        limit = 1 / math.sqrt(n_features)
+        self.w = np.random.uniform(-limit, limit, (n_features,))
+
+    def fit(self, X, y):
+        X = np.insert(X, 0, 1, axis=1)
+        self.training_errors = []
+        self.initialize_weights(n_features=X.shape[1])
+
+        for i in range(self.n_iterations):
+            y_pred = X.dot(self.w)
+            mse = np.mean(0.5 * (y - y_pred) ** 2 + self.regularization(self.w))
+            self.training_errors.append(mse)
+            grad_w = -(y - y_pred).dot(X) / X.shape[0] + self.regularization.grad(self.w)
+            self.w -= self.learning_rate * grad_w
+
+    def predict(self, X):
+        X = np.insert(X, 0, 1, axis=1)
+        y_pred = X.dot(self.w)
+        return y_pred
+    
+
+class LinearRegression(Regression):
+    """
+    Linear model.
+    Parameters:
+        n_iterations: float. The number of training iterations the algorithm will tune the weights for.
+        learning_rate: float. The step length that will be used when updating the weights.
+        gradient_descent: boolean. True or false depending if gradient descent should be used when training.
+                          If false then we use batch optimization by least squares.
+    """
+
+    def __int__(self, n_iterations=100, learning_rate=1e-3, gradient_descent=True):
+        super().__init__(n_iterations=n_iterations, learning_rate=learning_rate)
+        self.gradient_descent = gradient_descent
+        self.regularization = lambda x: 0
+        self.regularization.grad = lambda x: 0
+
+    def fit(self, X, y):
+        if not self.gradient_descent:
+            X = np.insert(X, 0, 1, axis=1)
+            U, S, V = np.linalg.svd(X.T.dot(X))
+            S = np.diag(S)
+            X_sq_reg_inv = V.dot(np.linalg.pinv(S)).dot(U.T)
+            self.w = X_sq_reg_inv.dot(X.T).dot(y)
+        else:
+            super().fit(X, y)
+
 
 
 
